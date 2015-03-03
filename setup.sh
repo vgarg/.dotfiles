4edd868316ffe7f5abcb62ff93ded2aa323c8866
Vivek Garg <vivek@cazena.com>
2015-03-02 21:15:27 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,3 +16,12 @@ mkdir ~/.vim/bundle
 #Install vundle
 echo "installing Vundle"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+#Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Install silver searcher
+brew install the_silver_searcher
+
+#Install git completion
+brew install git bash-completion
